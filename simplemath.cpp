75d53018a2cf5631a04d1ad62ebd89d7f35b7c81
Loading <imloadinq7579@gmail.com>
2022-06-29 16:26:53 -0500

    added simple math :D

---
+++ simplemath.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int one = 1;
+
+    cout << one << endl << endl;
+
+    int onePlusTwo = one + 2;
+
+    cout << onePlusTwo;
+
+    return 0;
+}
